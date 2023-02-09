d196772a5dca3b3723ccd6d6ba8282b2baceea03
neztach <36271369+neztach@users.noreply.github.com>
2023-02-08 19:34:49 -0600

    Update powershell_Get.ps1

--- powershell_Get.ps1
+++ powershell_Get.ps1
@@ -1,4 +1,20 @@
-$script:tempPath = -join ($env:TEMP, '\ws_setup')
+$script = $MyInvocation.MyCommand.Definition
+$ps     = Join-Path $PSHome 'powershell.exe'
+
+$isLocalAdmin = [bool]((net localgroup administrators) -match "$env:USERDOMAIN\\$env:USERNAME")
+
+If ($isLocalAdmin) {
+    If (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+        Start-Process $ps -Verb runas -ArgumentList "& '$script'"
+        exit
+    }
+} Else {
+    Start-Process $ps -ArgumentList (@('-File', $script) + $args) -Credential $credential
+    exit
+}
+
+$script:tempPath = 'C:\down\ws_setup'
+
 Write-Verbose -Message 'Verifying temp dir...'
 If (-not (Test-Path $script:tempPath)) {
     Write-Verbose -Message '    CREATING temp dir.'
@@ -6,6 +22,7 @@ If (-not (Test-Path $script:tempPath)) {
 } Else {
     Write-Verbose -Message '    VERIFIED Temp.'
 }
+
 
 Function Invoke-RemoteScript {
     [CmdletBinding()]
@@ -22,5 +39,5 @@ Set-Alias -Name irs -Value Invoke-RemoteScript
 irs 'https://aka.ms/install-powershell.ps1'
 Invoke-RestMethod 'https://raw.githubusercontent.com/neztach/EmDeploy/main/ws_setup.ps1' -OutFile $script:tempPath\ws_setup.ps1
 
-$pwsh = "$env:LOCALAPPDATA\Microsoft\powershell\pwsh.exe"
-. $pwsh -nologo -noprofile -file $script:tempPath\ws_setup.ps1 -verb runas
+$pwsh = "$env:LOCALAPPDATA\Microsoft\powershell-daily\pwsh.exe"
+. $pwsh -nologo -noprofile -file $script:tempPath\ws_setup.ps1
