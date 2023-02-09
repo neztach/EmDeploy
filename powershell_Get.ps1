00f7471ee770fb78700324848aac90dd0832f459
neztach <36271369+neztach@users.noreply.github.com>
2023-02-08 19:49:24 -0600

    Update powershell_Get.ps1

--- powershell_Get.ps1
+++ powershell_Get.ps1
@@ -1,17 +1,10 @@
 $script = $MyInvocation.MyCommand.Definition
 $ps     = Join-Path $PSHome 'powershell.exe'
 
-$isLocalAdmin = [bool]((net localgroup administrators) -match "$env:USERDOMAIN\\$env:USERNAME")
-
-If ($isLocalAdmin) {
-    If (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-        Start-Process $ps -Verb runas -ArgumentList "& '$script'"
-        exit
-    }
-}# Else {
-#    Start-Process $ps -ArgumentList (@('-File', $script) + $args) -Credential $credential
-#    exit
-#}
+If (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""iex ""& { $(irm 'https://raw.githubusercontent.com/neztach/EmDeploy/main/powershell_Get.ps1') }""""'}"
+    exit
+}
 
 $script:tempPath = 'C:\down\ws_setup'
 
